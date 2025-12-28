2eaedc17d60f94387e97023f550e9cc71ebefe08
brianwaithaka <waithakabrian8@gmail.com>
2025-12-28 09:35:59 +0300

    complete lab

--- lib/person.py
+++ lib/person.py
@@ -3,5 +3,13 @@
 class Person:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    # Instance method definition
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+
+# Add an instance method talk() to your Person class that will print "Hello World!"
+# Add an instance method walk() to your Person class that will print "The person is walking."
